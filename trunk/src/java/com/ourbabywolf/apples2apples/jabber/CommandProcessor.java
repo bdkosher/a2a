2f5c3ce56f9dd3c6457d18ad00ae4209c8f12dff
joewolf <joewolf@33bc20a0-ac1a-0410-8ccf-c1579c0fc6ed>
2006-12-28 02:23:06 +0000

    beginnings of jabber interaction

---
+++ trunk/src/java/com/ourbabywolf/apples2apples/jabber/CommandProcessor.java
@@ -0,0 +1,25 @@
+/*
+ * CommandProcessor.java - created Sep 1, 2006 12:53:48 AM
+ * $Id$
+ */
+package com.ourbabywolf.apples2apples.jabber;
+
+/**
+ * Generic interface for dealing with commands received through
+ * jabber.
+ * 
+ * @author joe@ourbabywolf.com
+ *
+ */
+public interface CommandProcessor {
+
+    /**
+     * Processes the given command from the given user.
+     * 
+     * @param command
+     * @param jabberId
+     * @param client
+     */
+    public void process(String command, String jabberId, HostingClient client);
+    
+}
